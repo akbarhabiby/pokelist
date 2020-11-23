4745b7994d8d7ba928572e563ef6edf9f3deb42a
akbarhabiby <akbarhabiby@icloud.com>
2020-11-23 22:41:28 +0700

    [Main] Changed Details button into anchor etc: Backup Details button

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -8,7 +8,8 @@ export default class Pokemon extends Component {
       <tr>
         <td className="align-middle">{poke.name[0].toUpperCase() + poke.name.slice(1)}</td>
         <td><img src={`https://pokeres.bastionbot.org/images/pokemon/${poke.url.split('/')[6]}.png`} alt={poke.name} width="100px" /></td>
-        <td className="align-middle"><button className="btn btn-primary">Details</button></td>
+        <td className="align-middle"><a href={poke.url} target="_blank" rel="noreferrer" className="btn btn-primary">Details</a></td>
+        {/* <td className="align-middle"><button className="btn btn-primary">Details</button></td> */}
       </tr>
     )
   }
