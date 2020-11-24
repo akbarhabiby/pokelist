d953c5095f6e453b172122653b11a66ae032364d
akbarhabiby <akbarhabiby@icloud.com>
2020-11-24 23:30:38 +0700

    [Main] Makes Project Folder more Structured 1. Created index.jsx on components folder for exporting each components 2. Created index.jsx on hooks folder for exporting each custom Hooks 3. Created styles and components folder 4. Renamed style.css into index.css and moved it into styles folder 5. Renamed import style.css into index.css in src/index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
-import Navbar from './Navbar'
-import Greeting from './Greeting'
-import PokemonTable from './PokemonTable'
-import useFetchPokemon from './hooks/useFetchPokemon'
+import { Navbar, Greeting, PokemonTable, Loading, ErrorPage } from './components/'
+import { useFetchPokemon } from './hooks/'
 
 export default function App() {
   // * Custom Hooks
   const [pokemons, loading, error] = useFetchPokemon()
 
-  if (loading) return (
-    <div className="d-flex justify-content-center" style={{ height: 100+'vh'}}>
-      <div className="align-self-center">
-        <div className="spinner-border text-info" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-      </div>
-    </div>
-  )
-
-  if (error) return (
-    <>
-      <center><h1>{error}</h1></center>
-      <hr/>
-      <center>Support: <a href="mailto:akbarhabiby@icloud.com">akbarhabiby@icloud.com</a></center>
-    </>
-  )
+  // * Show Loading Spinner
+  if (loading) return <Loading />
+  // * Show Error Page
+  if (error) return <ErrorPage error={error} />
 
   return (
     <>
